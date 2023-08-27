3295d34d9cdf820f145526fb417d881422459f3a
NourHassann <142754500+NourHassann@users.noreply.github.com>
2023-08-27 19:26:10 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
+from pickle import TRUE
 import socket as sock
 
 
-Host = '192,168,1,6'
+Host = '192.168.1.6'
 port = 2023
 
-server = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
-server.bind(Host, port)
-server.listen()
-client_socket , address = server.accept()
-message = client_socket.recv(1024).decode('utf-8')
+server = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
+server.bind((Host, port))
+while TRUE: 
+     message , address = server.recvfrom(1024)
+     print(message.decode('utf-8'))
+     server.sendto(f"packet{message.decode('utf-8')} is received".encode('utf-8'), address)
